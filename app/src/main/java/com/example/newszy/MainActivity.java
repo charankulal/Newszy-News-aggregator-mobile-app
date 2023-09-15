4023c9aae76aa6b37fb2d078854e135dad47d9f8
Charan <charankulal0241@gmail.com>
2023-09-15 22:32:18 +0530

    Created horizontal bar with button to switch the categories of the news

--- app/src/main/java/com/example/newszy/MainActivity.java
+++ app/src/main/java/com/example/newszy/MainActivity.java
@@ -7,16 +7,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.newszy.Models.NewsApiResponse;
 import com.example.newszy.Models.NewsHeadlines;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SelectListener{
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener{
     RecyclerView recyclerView;
     CustomAdapter adapter;
     ProgressDialog dialog;
+    Button b1,b2,b3,b4,b5,b6,b7;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,17 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
         dialog=new ProgressDialog(this);
         dialog.setTitle("Fetching News Articles..");
         dialog.show();
+
+        b1=findViewById(R.id.btn_1);
+        b2=findViewById(R.id.btn_2);
+        b3=findViewById(R.id.btn_3);
+        b4=findViewById(R.id.btn_4);
+        b5=findViewById(R.id.btn_5);
+        b6=findViewById(R.id.btn_6);
+        b7=findViewById(R.id.btn_7);
+
+
+
         RequestManager manager=new RequestManager(this);
         manager.getNewsHeadlines(listener,"general",null);
     }
@@ -53,5 +68,15 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
     @Override
     public void onNewsClicked(NewsHeadlines headlines) {
         startActivity(new Intent(MainActivity.this,DetailedNewsActivity.class).putExtra("data",headlines));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button=(Button) v;
+        String category=button.getText().toString();
+        dialog.setTitle("Fetching News Articles of "+ category);
+        dialog.show();
+        RequestManager manager=new RequestManager(this);
+        manager.getNewsHeadlines(listener,category,null);
     }
 }
