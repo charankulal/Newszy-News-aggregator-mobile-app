e9a72226b6fdf271ebb0f8e4c3aa7ac74090bf15
Charan <charankulal0241@gmail.com>
2023-10-08 08:31:14 +0530

    Implemented webview in DetailedNewsActivity

--- app/src/main/java/com/example/newszy/DetailedNewsActivity.java
+++ app/src/main/java/com/example/newszy/DetailedNewsActivity.java
@@ -2,37 +2,28 @@ package com.example.newszy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.webkit.WebView;
 
 import com.example.newszy.Models.NewsHeadlines;
-import com.squareup.picasso.Picasso;
 
 public class DetailedNewsActivity extends AppCompatActivity {
-    NewsHeadlines headlines;
-    TextView txt_title, txt_author,txt_view,txt_detail,txt_content,txt_time;
-    ImageView img_news;
+    private WebView webView;
+    NewsHeadlines newsHeadlines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_news);
 
-        txt_title=findViewById(R.id.text_detail_title);
-        txt_author=findViewById(R.id.text_detail_author);
-        txt_time=findViewById(R.id.text_detail_time);
-        txt_detail=findViewById(R.id.text_detail_detail);
-        txt_content=findViewById(R.id.text_detail_content);
-        img_news=findViewById(R.id.image_detail_news);
+        newsHeadlines=(NewsHeadlines) getIntent().getSerializableExtra("data");
 
-        headlines= (NewsHeadlines) getIntent().getSerializableExtra("data");
+        String url=newsHeadlines.getUrl();
 
-        txt_title.setText(headlines.getTitle());
-        txt_author.setText(headlines.getAuthor());
-        txt_time.setText(headlines.getPublishedAt());
-        txt_detail.setText(headlines.getDescription());
-        txt_content.setText(headlines.getContent());
-        Picasso.get().load(headlines.getUrlToImage()).into(img_news);
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
     }
 }
